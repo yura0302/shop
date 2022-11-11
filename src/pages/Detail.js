11ace963c4c7a022366376841c8c81fed3f9cd34
yura <rladbfk0000@naver.com>
2022-11-11 18:14:55 +0900

    fix : detail페이지 이미지 에러 해결

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -7,30 +7,29 @@ function Detail(props) {
     setTimeout(() => {
       setAlert(false);
     }, 1000);
-  });
+  }, []);
 
   const { id } = useParams();
-  const product = props.shoes.find(function (i) {
-    return i.id === id;
-  });
+  const product = props.shoes.find((i) => i.id == id);
+  console.log(product);
   return (
     <div className="container">
       {alert === true ? (
         <div className="alert alert-warning">1초 이내 구매 시 신발 공짜</div>
       ) : null}
-      ㄴ
       <div className="row">
         <div className="col-md-6">
           <img
             alt="#"
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            src={`https://codingapple1.github.io/shop/shoes${id * 0 + 1}.jpg`}
+            // 나누기는 숫자형으로 자동변환되서 계산됨 ==자동형변환
             width="100%"
           />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{product.title}</h4>
-          <p>{product.content}</p>
-          <p>{product.price}원</p>
+          <h4 className="pt-5">{product?.title}</h4>
+          <p>{product?.content}</p>
+          <p>{product?.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
